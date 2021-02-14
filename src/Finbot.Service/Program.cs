e97dd1249efebad2b1124725f89ea90edf615788
Jon <>
2021-02-14 04:48:57 -0500

    Changes for config

--- src/Finbot.Service/Program.cs
+++ src/Finbot.Service/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Finbot.Core;
 using Finbot.Core.IEX;
 using Finbot.Service;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,23 +23,42 @@ namespace finbot
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                var env = hostContext.HostingEnvironment;
+
+                config.AddEnvironmentVariables();
+
+                if (env.IsDevelopment())
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                    if (appAssembly != null)
+                    {
+                        config.AddUserSecrets(appAssembly, optional: true);
+                    }
+                }
+            })
             .ConfigureServices((hostContext, services) =>
             {
+                var config = hostContext.Configuration;
+
                 services.AddSingleton<IPortfolioManager, PortfolioManager>();
 
                 services.AddTransient<IFinDataClient, FinDataClient>(
                     serviceManager =>
                     {
-                        var uri = Environment.GetEnvironmentVariable("iex.uri");
-                        var restCLient = new RestClient(uri);
-                        restCLient.Authenticator = new TokenAuthenticator(Environment.GetEnvironmentVariable("iex.token"));
+                        var restCLient = new RestClient(config.GetValue<string>("iex:uri"))
+                        {
+                            Authenticator = new TokenAuthenticator(config.GetValue<string>("iex:token"))
+                        };
+
                         return new FinDataClient(restCLient, serviceManager.GetService<ILogger>());
                     });
 
                 services.AddTransient<IFinbotBrain, FinbotBrain>(
                     serviceManager =>
                     {
-                        var token = Environment.GetEnvironmentVariable("discord.token");
+                        var token = config.GetValue<string>("discord:token");
                         var finCLient = serviceManager.GetService<IFinDataClient>();
                         var logger = serviceManager.GetService<ILogger<FinbotBrain>>();
                         var portfolioManager = serviceManager.GetService<IPortfolioManager>();
